516844f78c0c71b2995e416e476294c37c6ecfb2
Jingda Mai <jingdamai@gmail.com>
2019-12-07 23:20:46 +0900

    final

---
+++ components/image.js
@@ -0,0 +1,5 @@
+const Image = () => {
+  return <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Indian_Chameleon_%28Chamaeleo_zeylanicus%29_Photograph_By_Shantanu_Kuveskar.jpg/440px-Indian_Chameleon_%28Chamaeleo_zeylanicus%29_Photograph_By_Shantanu_Kuveskar.jpg' alt='chameleon'/>
+};
+
+export default Image;
